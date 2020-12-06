731aba31cdd56291eefe666ffafecc6a4a213358
Russell Brown <33341699+RussellJBrown@users.noreply.github.com>
2020-12-06 15:34:45 -0800

    Add files via upload

---
+++ wordManipulation.py
@@ -0,0 +1,14 @@
+import json
+def removeCommonWords():
+    f = open('intent.json','w')
+    #data = json.load(f)
+    #for i in data['Hello']:
+    #    print("a")
+    #    print(i)
+
+    f.close()
+
+
+
+if __name__ == '__main__':
+    removeCommonWords()
